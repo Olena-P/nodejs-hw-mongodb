06fad8791d93406e2a2a1edfc452bde2583d01ba
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2024-06-16 10:45:14 +0300

    Improve input validation: Add validateBody middleware and validation schemas for POST and PATCH /contacts routes

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -2,6 +2,12 @@ import { Router } from 'express';
 import * as contactsController from '../controllers/contacts.js';
 
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contactSchemas.js';
 
 const router = Router();
 
@@ -11,18 +17,23 @@ router.get(
 );
 router.get(
   '/contacts/:contactId',
+  isValidId,
   ctrlWrapper(contactsController.getContactByIdController),
 );
 router.post(
   '/contacts',
+  validateBody(createContactSchema),
   ctrlWrapper(contactsController.createContactController),
 );
 router.patch(
   '/contacts/:contactId',
+  isValidId,
+  validateBody(updateContactSchema),
   ctrlWrapper(contactsController.updateContactController),
 );
 router.delete(
   '/contacts/:contactId',
+  isValidId,
   ctrlWrapper(contactsController.deleteContactController),
 );
 
